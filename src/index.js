93fbb7b808f5b3a73c1282e85a5de03a190b448c
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-08 15:57:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
+import { Profile } from 'components/Profile';
+// import { Statistics } from 'components/Statistics';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  
+    <Profile />
   </React.StrictMode>
 );
