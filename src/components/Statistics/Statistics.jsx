7f8ffec40bd690bc34b65ef4f17e2240b204dec1
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-09 23:38:38 +0200

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+
+const Statistics = ({title, stats}) => {
+  return (
+    <section class="statistics">
+  <h2 class="title">{title}</h2>
+  <ul class="stat-list">
+    {stats.map( stat => (
+      <li class="item" key= {stat.id}>
+      <span class="label">{stat.label}</span>
+      <span class="percentage">{stat.percentage}</span>
+    </li>
+    ))
+    }
+    
+  </ul>
+</section>
+  )
+}
+
+export default Statistics;
+
+Statistics.propTypes = {
+  id: PropTypes.node.isRequired,
+  label: PropTypes.string.isRequired,
+  percentage: PropTypes.number.isRequired,
+};
