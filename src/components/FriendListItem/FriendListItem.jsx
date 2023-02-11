b87c9ef50ec981d3c4716b6478d3eefd89d7ddd8
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-11 14:22:12 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
+import styles from './FriendListItem.module.css'
 
 const FriendListItem = ({avatar, name, isOnline}) => {
     return (
-        <li class="item">
-        <span class="status">{isOnline}</span>
-        <img class="avatar"  src={avatar} alt="User avatar" width="48" />
-        <p class="name">{name}</p>
+        <li className={styles.item}>
+        <span className={styles.status}>{isOnline}</span>
+        <img className={styles.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={styles.name}>{name}</p>
       </li>
     )
   }
@@ -16,5 +17,5 @@ const FriendListItem = ({avatar, name, isOnline}) => {
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number.isRequired,
+    id: PropTypes.number,
   };
