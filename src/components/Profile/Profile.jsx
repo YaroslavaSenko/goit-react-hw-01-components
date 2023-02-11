e7b11504c5c8f8fad4ca8905fb7ba25077fea49b
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-11 14:22:21 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,31 @@
 import PropTypes from "prop-types";
+import styles from './Profile.module.css'
 
 const Profile = ({username, tag, location, avatar, stats}) => {
   return (
-    <div class="profile">
-  <div class="description">
+    <div className={styles.profile}>
+  <div className={styles.description}>
     <img
       src= {avatar}
       alt="User avatar"
-      class="avatar"
+      className={styles.avatar}
     />
-    <p class="name">{username}</p>
-    <p class="tag">@{tag}</p>
-    <p class="location">{location}</p>
+    <p className={styles.name}>{username}</p>
+    <p className={styles.tag}>@{tag}</p>
+    <p className={styles.location}>{location}</p>
   </div>
-  <ul class="stats">
+  <ul className={styles.stats}>
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">{stats.followers}</span>
+      <span className={styles.label}>Followers</span>
+      <span className={styles.quantity}>{stats.followers}</span>
     </li> 
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">{stats.views}</span>
+      <span className={styles.label}>Views</span>
+      <span className={styles.quantity}>{stats.views}</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">{stats.likes}</span>
+      <span className={styles.label}>Likes</span>
+      <span className={styles.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
@@ -37,7 +38,7 @@ export default Profile;
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
-  location: PropTypes.number.isRequired,
+  location: PropTypes.string.isRequired,
   avatar:PropTypes.string.isRequired,
-  stats: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
