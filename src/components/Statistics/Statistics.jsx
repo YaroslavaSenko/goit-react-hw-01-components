05a803f077baf4dad624d83cbf1f41e84641504e
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-15 16:21:22 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,21 @@
 import PropTypes from "prop-types";
-import styles from './Statistics.module.css'
+import {StatisticsCard, Title, StatList, Item, Label, Percentage } from './Statistacs.styled';
 
 const Statistics = ({title, stats}) => {
   return (
-    <section className={styles.statistics}>
-  <h2 className={styles.title}>{title}</h2>
-  <ul className={styles.statList}>
+    <StatisticsCard>
+  <Title>{title}</Title>
+  <StatList>
     {stats.map( stat => (
-      <li className={styles.item} key= {stat.id}>
-      <span className={styles.label}>{stat.label}</span>
-      <span className={styles.percentage}>{stat.percentage}%</span>
-    </li>
+      <Item key= {stat.id}>
+      <Label>{stat.label}</Label>
+      <Percentage>{stat.percentage}%</Percentage>
+    </Item>
     ))
     }
     
-  </ul>
-</section>
+  </StatList>
+</StatisticsCard>
   )
 }
 
