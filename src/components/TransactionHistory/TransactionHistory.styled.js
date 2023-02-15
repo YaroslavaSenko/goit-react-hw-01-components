15274d5f945eff79a59de5a3fd99a5189a548cd8
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-15 16:21:27 +0200

    Create TransactionHistory.styled.js

---
+++ src/components/TransactionHistory/TransactionHistory.styled.js
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+export const TransactionsHistory = styled.table`
+margin-top: 20px;   
+padding: 10px 10px;
+width: 370px;
+margin-left: auto;
+margin-right: auto;
+background-color: white;
+border: 1px solid #d8e4e2;
+border-radius: 8px;
+  box-shadow: rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px,
+  rgb(0 0 0 / 12%) 0px 1px 8px 0px;
+ justify-content: center;
+ text-align: center;
+  
+`;
