567d63909702477ca17cab5fd8c79d75fb219a43
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-02-15 16:21:30 +0200

    Update Transactions.jsx

--- src/components/TransactionHistory/Transactions.jsx
+++ src/components/TransactionHistory/Transactions.jsx
@@ -1,1 +1,36 @@
 import PropTypes from "prop-types";
+import {TransactionsHistory} from './TransactionHistory.styled';
+
+const Transactions = ({items }) => {
+    return (
+        <TransactionsHistory >
+     <thead >
+     <tr>
+        <th>Type </th>
+        <th>Amount </th>
+       <th>Currency </th>
+       </tr>
+       </thead>
+      <tbody>
+        {items.map( item =>(
+    <tr>
+      <td key= {item.id}>{item.type}</td>
+      <td >{item.amount}</td>
+      <td>{item.currency}</td>
+    </tr>
+        ))}
+    
+  </tbody> 
+  
+    </TransactionsHistory>
+    )
+  }
+  export default Transactions;
+
+  Transactions.propTypes = {
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency:PropTypes.string.isRequired,
+  };
+  
